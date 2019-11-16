66d8fdc908e3395728c05e2b74fb6f4094c3a7a3
Mohamadhassan <emohamadhassan@gmail.com>
2019-11-16 14:39:57 +0330

    Updates

---
+++ precache-manifest.12701ed298e27d555476b3f304f13f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a45b02336a7b454beea3800eb4db677",
+    "url": "/add-calc/index.html"
+  },
+  {
+    "revision": "e3f55458af5722861109",
+    "url": "/add-calc/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3b51ca7c3cd8ce29a709",
+    "url": "/add-calc/static/js/2.e31a3735.chunk.js"
+  },
+  {
+    "revision": "e3f55458af5722861109",
+    "url": "/add-calc/static/js/main.f3c3bf0a.chunk.js"
+  },
+  {
+    "revision": "d6d04be21585f0c938ee",
+    "url": "/add-calc/static/js/runtime-main.90725e16.js"
+  }
+]);
